7f1a0f4bceaf882c9fb56fb33c71848f240a4aa0
Rechdi-Madjidi <13yassin@gmail.com>
2025-10-28 10:58:15 +0100

    deleting the summarizin
    Removed unnecessary summary comments from the PptCl class.

--- PptCl.Designer.cs
+++ PptCl.Designer.cs
@@ -2,15 +2,9 @@
 {
     partial class PptCl
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+// deleting the summarizin from  here
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -91,3 +85,4 @@
     }
 
 }
+
